eb6e048943a4d2e65b493e49d01258b5833000bd
ajf012 <128007145+ajf012@users.noreply.github.com>
2023-03-16 10:19:20 +0900

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,7 @@
+Transverse = float(input("transverse:"))
+Length = float(input("length:"))
+Height = float(input("height:"))
+
+Volume = Transverse * Length * Height
+
+print("The volume of the box is:", Volume)
