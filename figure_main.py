7ed5b6596873f09af82f0b8b4f241ec408485a36
ajf012 <ajf012@naver.com>
2023-06-01 13:08:39 +0900

    implement hw5

--- figure_main.py
+++ figure_main.py
@@ -1,42 +1,42 @@
 """
 class line을 불러온 뒤 __length 값을 set_length(), get_length()를 이용해 받고 
-이를 area_square(), area_regular_triangle(), area_circle()의 매개변수로 설정하여
-각각 정사각형의 넓이, 정삼각형의 넓이, 원의 넓이를 구한다.
+이를 area_rectangle(), area_regular_triangle(), area_ellipse()의 매개변수로 설정하여
+각각 직사각형의 넓이, 직각삼각형의 넓이, 타원의 넓이를 구한다.
 """
+
 import figure
 """
 figure 모듈 전체를 가져온다.
 """
+myline = figure.line(10, 20)
+"""
+class line을 불러온 뒤 __width=10, __height=20으로 설정한다.
+"""
+width, height = myline.get_length()
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
+"""
+직사각형 넓이를 구하는 함수를 매개변수로 받은 width나 height의 값이 0이하일 경우 ValueError를 발생시킨다."""
 
-myline = figure.line(10)
+myline.set_length(20, 30)
+width, height = myline.get_length()
+try:
+    triangle = figure.area_regular_triangle(width, height)
+    print(triangle)
+except ValueError:
+    print("please input positive number for width and height")
 """
-class line을 불러온 뒤 __length=10으로 설정한다.
-"""
+직각삼각형 넓이를 구하는 함수를 매개변수로 받은 width나 height의 값이 0이하일 경우 ValueError를 발생시킨다."""
 
-square = figure.area_square(myline.get_length())
-print(square)
+myline.set_length(30, 40)
+width, height = myline.get_length()
+try:
+    ellipse = figure.area_ellipse(width, height)
+    print(ellipse)
+except ValueError:
+    print("please input positive number for width and height")
 """
-get_length()를 이용하여 __length의 값을 받고
-이를 정사각형의 넓이를 구하는 함수 area_square()의 
-매개변수로 설정하여 정사각형의 넓이를 구한다.
-"""
-
-myline.set_length(20)
-regular_triangle = figure.area_regular_triangle(myline.get_length())
-print(regular_triangle)
-"""
-set_length()를 이용하여 __length의 값을 수정하고
-get_length()를 이용하여 __length의 값을 받은 뒤
-이를 정삼각형의 넓이를 구하는 함수 area_regular_triangle()의
-매개변수로 설정하여 정삼각형의 넓이를 구한다.
-"""
-
-myline.set_length(30)
-circle = figure.area_circle(myline.get_length())
-print(circle)
-"""
-set_length()를 이용하여 __length의 값을 수정하고
-get_length()를 이용하여 __length의 값을 받은 뒤
-이를 원의 넓이를 구하는 함수 area_circle()의 
-매개변수로 설정하여 원의 넓이를 구한다.
-"""
+타원의 넓이를 구하는 함수를 매개변수로 받은 width나 height의 값이 0이하일 경우 ValueError를 발생시킨다."""
